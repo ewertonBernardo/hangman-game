0f20ba77bff49845438e378a755f752361e21b64
ewertonBernardo <ewerton.learning@gmail.com>
2023-01-21 13:11:35 -0300

    check_letter

--- main.py
+++ main.py
@@ -13,13 +13,17 @@ def show_letters(letters):
 	print('\n')
 
 
+def check_letter(word, letter):
+	if (letter in word):
+		return True
+
 def check_word(word, letters):
 	letters = "".join(letters)
 	if (word == letters):
 		return True
 
 
-
+remaining_attempts  = 5
 word = input('What\'s the word?').upper()
 
 
@@ -27,14 +31,20 @@ letters = undescore_letters(word)
 show_letters(letters)
 
 
-while True:
+while True and remaining_attempts > 0:
 	attempt = input('Choose a letter: ').upper()
 	for i,l in enumerate(word):
-		if(attempt == l):
+		if (attempt == l):
 			#print(i)
 			letters[i] = attempt
 
 	show_letters(letters)
-	if(check_word(word, letters)):
+
+	if (check_letter(word, attempt) == None):
+		remaining_attempts -= 1
+		print(f'Remaining attempts: {remaining_attempts}')
+
+	if (check_word(word, letters)):
 		print("HIT!")
 		break
+	
